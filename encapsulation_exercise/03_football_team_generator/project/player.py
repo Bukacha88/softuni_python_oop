729665f1486b97b8407adb257ece664bc1d87079
Ivan <mzifa@abv.bg>
2021-03-06 16:01:23 +0200

    exercise number 3

---
+++ encapsulation_exercise/03_football_team_generator/project/player.py
@@ -0,0 +1,61 @@
+class Player:
+    def __init__(self, name, endurance, sprint, dribble, passing, shooting):
+        self.__shooting = shooting
+        self.__passing = passing
+        self.__dribble = dribble
+        self.__sprint = sprint
+        self.__endurance = endurance
+        self.__name = name
+
+    @property
+    def shooting(self):
+        return self.__shooting
+
+    @shooting.setter
+    def shooting(self, value):
+        self.__shooting = value
+
+    @property
+    def passing(self):
+        return self.__passing
+
+    @passing.setter
+    def passing(self, value):
+        self.__passing = value
+
+    @property
+    def dribble(self):
+        return self.__dribble
+
+    @dribble.setter
+    def dribble(self, value):
+        self.__dribble = value
+
+    @property
+    def sprint(self):
+        return self.__sprint
+
+    @sprint.setter
+    def sprint(self, value):
+        self.__sprint = value
+
+    @property
+    def endurance(self):
+        return self.__endurance
+
+    @endurance.setter
+    def endurance(self, value):
+        self.__endurance = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    def __str__(self):
+        return f"Player: {self.__name}\nEndurance: {self.__endurance}\n" \
+               f"Sprint: {self.__sprint}\nDribble: {self.__dribble}\nPassing: " \
+               f"{self.__passing}\nShooting: {self.__shooting}\n"
---
+++ encapsulation_exercise/03_football_team_generator/project/team.py
@@ -0,0 +1,48 @@
+class Team:
+    def __init__(self, name, rating):
+        self.__rating = rating
+        self.__name = name
+        self.__players = []
+
+    @staticmethod
+    def find_player(player_name, players):
+        return [player for player in players if player.name == player_name]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        self.__rating = value
+
+    @property
+    def players(self):
+        return self.__players
+
+    @players.setter
+    def players(self, value):
+        self.__players = value
+
+    def add_player(self, player):
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name):
+        if not self.find_player(player_name, self.__players):
+            return f"Player {player_name} not found"
+        player = self.find_player(player_name, self.__players)[0]
+        self.__players.remove(player)
+        return player
+
+
