7969146257e9664e2036cfec52324ed0a0f31428
Ivan <mzifa@abv.bg>
2021-03-15 14:59:53 +0200

    exercise

---
+++ polymorphism_exercise/04_wild_farm/project/animals/birds.py
@@ -0,0 +1,28 @@
+from project.animals.animal import Bird
+from project.food import Meat, Vegetable, Fruit
+
+
+class Owl(Bird):
+    WEIGHT_INCREASE = 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+    def feed(self, food):
+        if not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.weight += Owl.WEIGHT_INCREASE * food.quantity
+        self.food_eaten += food.quantity
+
+
+class Hen(Bird):
+    WEIGHT_INCREASE = 0.35
+
+    def make_sound(self):
+        return "Cluck"
+
+    def feed(self, food):
+        self.weight += Hen.WEIGHT_INCREASE * food.quantity
+        self.food_eaten += food.quantity
+
+
