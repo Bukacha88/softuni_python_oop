7cb57c06fa956fb7c0de19502945adfe712b0e04
Ivan Ivanov <bukacha9@gmail.com>
2021-04-07 20:36:50 +0300

    exam prep

---
+++ exam_prep/python_oop_retake_exam_19_dec_2020/project/medicine/painkiller.py
@@ -0,0 +1,8 @@
+from exam_prep.python_oop_retake_exam_19_dec_2020.project.medicine import Medicine
+
+
+class Painkiller(Medicine):
+    def __init__(self):
+        super().__init__(health_increase=20)
+
+
