612ad02ae52662eedb9ba1958de6c163ee3edbaa
Ivan Ivanov <bukacha9@gmail.com>
2021-04-10 18:09:04 +0300

    exam

---
+++ exam_prep/exam_10_april_2021/project/decoration/ornament.py
@@ -0,0 +1,6 @@
+from exam_prep.exam_10_april_2021.project.decoration import BaseDecoration
+
+
+class Ornament(BaseDecoration):
+    def __init__(self):
+        super().__init__(comfort=1, price=5)
